29fa8159aaf3fa6014cec439fddee03c8a3cd16f
yoshidaei <yoshidaei@262b1df3-3705-4e7d-84bc-ae5d46f3fe09>
2011-03-21 07:28:46 +0000

    git-svn-id: http://svn.code.sf.net/p/ermaster/svn@33 262b1df3-3705-4e7d-84bc-ae5d46f3fe09

--- branches/https:/subversion.assembla.com/svn/ermaster/branches/org.insightech.er/src/org/insightech/er/db/impl/hsqldb/HSQLDBPreTableImportManager.java
+++
@@ -1,7 +0,0 @@
-package org.insightech.er.db.impl.hsqldb;
-
-import org.insightech.er.editor.model.dbimport.PreImportFromDBManager;
-
-public class HSQLDBPreTableImportManager extends PreImportFromDBManager {
-
-}
