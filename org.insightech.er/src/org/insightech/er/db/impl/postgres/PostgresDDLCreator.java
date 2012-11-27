fd33edf85788ba36ad800d0de6359505e5f9485e
h_nakajima <h_nakajima@262b1df3-3705-4e7d-84bc-ae5d46f3fe09>
2012-11-27 14:22:56 +0000

    git-svn-id: http://svn.code.sf.net/p/ermaster/svn@70 262b1df3-3705-4e7d-84bc-ae5d46f3fe09

--- org.insightech.er/src/org/insightech/er/db/impl/postgres/PostgresDDLCreator.java
+++ org.insightech.er/src/org/insightech/er/db/impl/postgres/PostgresDDLCreator.java
@@ -219,15 +219,17 @@ public class PostgresDDLCreator extends DDLCreator {
 		boolean first = true;
 
 		for (NormalColumn column : table.getNormalColumns()) {
-			if (SqlType.SQL_TYPE_ID_SERIAL.equals(column.getType().getId())
-					|| SqlType.SQL_TYPE_ID_BIG_SERIAL.equals(column.getType()
-							.getId())) {
-				String autoIncrementSettingDDL = getAutoIncrementSettingDDL(
-						table, column);
-				if (!Check.isEmpty(autoIncrementSettingDDL)) {
-					ddl.append(autoIncrementSettingDDL);
-					ddl.append("\r\n");
-					first = false;
+			if (column.getType() != null) {
+				if (SqlType.SQL_TYPE_ID_SERIAL.equals(column.getType().getId())
+						|| SqlType.SQL_TYPE_ID_BIG_SERIAL.equals(column
+								.getType().getId())) {
+					String autoIncrementSettingDDL = getAutoIncrementSettingDDL(
+							table, column);
+					if (!Check.isEmpty(autoIncrementSettingDDL)) {
+						ddl.append(autoIncrementSettingDDL);
+						ddl.append("\r\n");
+						first = false;
+					}
 				}
 			}
 		}
