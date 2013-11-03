6510d824a6bd49086c0085d9c091b43797fccd5d
h_nakajima <h_nakajima@262b1df3-3705-4e7d-84bc-ae5d46f3fe09>
2013-11-03 04:58:15 +0000

    For the performance improvement, I improved how to use PropertyChangeEvent completely.
    git-svn-id: http://svn.code.sf.net/p/ermaster/svn@84 262b1df3-3705-4e7d-84bc-ae5d46f3fe09

---
+++ org.insightech.er/src/org/insightech/er/editor/controller/command/display/ChangeFontCommand.java
@@ -0,0 +1,50 @@
+package org.insightech.er.editor.controller.command.display;
+
+import org.insightech.er.editor.controller.command.AbstractCommand;
+import org.insightech.er.editor.model.ViewableModel;
+
+public class ChangeFontCommand extends AbstractCommand {
+
+	private ViewableModel viewableModel;
+
+	private String oldFontName;
+
+	private String newFontName;
+
+	private int oldFontSize;
+
+	private int newFontSize;
+
+	public ChangeFontCommand(ViewableModel viewableModel, String fontName,
+			int fontSize) {
+		this.viewableModel = viewableModel;
+
+		this.oldFontName = viewableModel.getFontName();
+		this.oldFontSize = viewableModel.getFontSize();
+
+		this.newFontName = fontName;
+		this.newFontSize = fontSize;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void doExecute() {
+		this.viewableModel.setFontName(this.newFontName);
+		this.viewableModel.setFontSize(this.newFontSize);
+		
+		this.viewableModel.refreshFont();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected void doUndo() {
+		this.viewableModel.setFontName(this.oldFontName);
+		this.viewableModel.setFontSize(this.oldFontSize);
+		
+		this.viewableModel.refreshFont();
+	}
+}
